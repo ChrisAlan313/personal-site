6d84449598ee231933493a8d91e8aa2ca12c7305
Chris Booty <chrisalan313@gmail.com>
2023-08-13 13:15:15 -0400

    Add eslint and go by default airbnb style guide

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
  * Initial Setup
  */
 
-'use strict';
 const parser = new DOMParser();
 
 /**
@@ -10,8 +9,8 @@ const parser = new DOMParser();
  */
 
 const elements = {
-    getWeatherButton: document.getElementById('getWeather'),
-    weatherCardContainer: document.getElementById('weatherCardContainer'),
+  getWeatherButton: document.getElementById('getWeather'),
+  weatherCardContainer: document.getElementById('weatherCardContainer'),
 };
 
 /**
@@ -19,7 +18,7 @@ const elements = {
  */
 
 const createWeatherAlertCard = (alertData, index) => {
-    const alertCard = `
+  const alertCard = `
         <div id="alertCard-${index}" class="alertCard">
             <h3 class="alertTitle">${alertData.properties.event}</h3>
             <p class="alertArea">${alertData.properties.areaDesc}</p>
@@ -30,58 +29,60 @@ const createWeatherAlertCard = (alertData, index) => {
             </div>
         </div>`;
 
-    return parser.parseFromString(alertCard, 'text/html').body.firstChild;
+  return parser.parseFromString(alertCard, 'text/html').body.firstChild;
 };
 
 const createErrorCard = (errorMessage) => {
-    const errorCard = `
+  const errorCard = `
         <div id="errorCard">
             <p id="errorDescription">${errorMessage}</p>
         </div>`;
 
-    return parser.parseFromString(errorCard, 'text/html').body.firstChild;
+  return parser.parseFromString(errorCard, 'text/html').body.firstChild;
 };
 
 /**
  * Event Handlers
  */
 
-const handleExpandDetailsClick = (event) => {
-    const currentBook = event.target.nextElementSibling.hidden;
-    event.target.nextElementSibling.hidden = !currentBook;
+// Set hidden if not hidden, and vice versa
+const handleExpandDetailsClick = (e) => {
+  const event = e;
+  const currentBook = event.target.nextElementSibling.hidden;
+  event.target.nextElementSibling.hidden = !currentBook;
 };
 
 const handleGetWeatherClick = async () => {
-    const response = await fetch(
-        'https://api.weather.gov/alerts/active?area=FL'
-    );
+  const response = await fetch(
+    'https://api.weather.gov/alerts/active?area=FL',
+  );
 
-    if (response.ok) {
-        elements.getWeatherButton.disabled = true;
-        elements.getWeatherButton.remove();
+  if (response.ok) {
+    elements.getWeatherButton.disabled = true;
+    elements.getWeatherButton.remove();
 
-        const data = await response.json();
+    const data = await response.json();
 
-        // The response data's features property is an array of objects that
-        // contain the weather alert data. We iterate over that to create the
-        // alert cards from each.
-        const alertCards = data.features.map(createWeatherAlertCard);
-        alertCards.map((alertCard, index) => {
-            elements.weatherCardContainer.append(alertCard);
-            document
-                .getElementById(`expandDetailsButton-${index}`)
-                .addEventListener('click', handleExpandDetailsClick);
-        });
-    } else {
-        if (document.getElementById('errorCard')) {
-            document.getElementById('errorCard').remove();
-        }
-
-        const errorMessage = `An error has occured: ${response.status}`;
-        const errorCard = createErrorCard(errorMessage);
-
-        elements.weatherCardContainer.appendChild(errorCard);
+    // The response data's features property is an array of objects that
+    // contain the weather alert data. We iterate over that to create the
+    // alert cards from each.
+    const alertCards = data.features.map(createWeatherAlertCard);
+    alertCards.map((alertCard, index) => {
+      elements.weatherCardContainer.append(alertCard);
+      return document
+        .getElementById(`expandDetailsButton-${index}`)
+        .addEventListener('click', handleExpandDetailsClick);
+    });
+  } else {
+    if (document.getElementById('errorCard')) {
+      document.getElementById('errorCard').remove();
     }
+
+    const errorMessage = `An error has occured: ${response.status}`;
+    const errorCard = createErrorCard(errorMessage);
+
+    elements.weatherCardContainer.appendChild(errorCard);
+  }
 };
 
 /**
